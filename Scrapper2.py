b5866344f784ea97e202751b25e41d2a2c0bb048
srimani-programmer <srimani.scholar@gmail.com>
2019-08-05 20:22:29 +0530

    About and More about scraped data code sucessful.

--- Scrapper2.py
+++ Scrapper2.py
@@ -9,8 +9,25 @@ soup = BeautifulSoup(res.text, 'lxml')
 
 placeName = soup.find_all('div', class_="col-md-12 col-xs-12 nopadding sectionBorder desktopFlex")
 
+# Getting the Place Name
 def getPlaceName():
     for i in placeName:
         print(i.h1.text)
 
-getPlaceName()
+#getPlaceName()
+
+about = soup.find_all('div',class_="col-md-8 col-xs-12 paddingSet")
+
+# getting the About Data
+def getAboutData():
+    for i in about:
+        data = i.find_all('div', class_='readMoreText')
+        for i in data:
+            print(i.p.text)
+
+#getAboutData()
+
+def getAboutMoreData():
+    for i in about:
+        data = i.find_all('p',attrs={'class':'textColor infoSpace'})
+        print(str(data[1])[32:len(data)-6])
